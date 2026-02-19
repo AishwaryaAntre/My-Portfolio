2d7aba56a1e44cc6c5ade857456af985861d67d7
AishwaryaAntre <aishwarya.antre21@gmail.com>
2026-02-19 23:59:11 +0530

    Update resume download link to new PDF file

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,7 @@ function Resume() {
     <section className="section">
       <h1>Resume</h1>
       <p>Download my resume to learn more about my experience and qualifications.</p>
-      <a href="/assets/My_Resume.pdf" download="My_Resume.pdf" className="resume-link">
+      <a href="/assets/Aishwarya-G_Antre.pdf" download="Aishwarya-G_Antre.pdf" className="resume-link">
         Download Resume (PDF)
       </a>
       {/* Optional: Embed PDF viewer */}
